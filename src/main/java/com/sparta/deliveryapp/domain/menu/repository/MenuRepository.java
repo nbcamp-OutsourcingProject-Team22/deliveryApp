9171f4b963f8946c5dfdc580e2f4fb90160f7659
vege4944 <vege4944@gmail.com>
2024-09-21 22:52:56 +0900

    fix. 메뉴 수정 1. MenuResponse 에 매개변수 생성자 추가 2. MenuRepository 에 findByStore 매서드 추가 3. 메뉴 코드 수정

--- src/main/java/com/sparta/deliveryapp/domain/menu/repository/MenuRepository.java
+++ src/main/java/com/sparta/deliveryapp/domain/menu/repository/MenuRepository.java
@@ -1,7 +1,9 @@
 package com.sparta.deliveryapp.domain.menu.repository;
 
 import com.sparta.deliveryapp.entity.Menu;
+import com.sparta.deliveryapp.entity.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MenuRepository extends JpaRepository<Menu, Long> {
+    Menu findByStore(Store store); // 메뉴 entity에 있는 스토어아이디로 메뉴를 가져오기 위해서
 }
